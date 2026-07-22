4ed091a91a33f762e5434f5118c03cd0bf572025
Ayaz Rahim <devops.mark@gmail.com>
2026-07-22 18:33:02 +0500

    modifying the bucket configuratin

--- terraform/s3bucket.tf
+++ terraform/s3bucket.tf
@@ -1,5 +1,12 @@
 provider "aws" {
   region = "us-east-1"
+
+  # Credentials are injected via environment variables by the CI pipeline:
+  # AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_SESSION_TOKEN
+  # Skip the STS GetCallerIdentity call that fails with dynamic Vault credentials
+  skip_credentials_validation = true
+  skip_requesting_account_id  = true
+  skip_metadata_api_check     = true
 }
 
 resource "random_id" "bucket_suffix" {
